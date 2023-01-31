18fe120df0917520ab75aba1200739bd55b4a1af
Guilherme Brumatti <guilhermebrumatti@yahoo.com.br>
2023-01-31 10:36:18 -0300

    Delete remote-state.tf

--- terraform/remote-state.tf
+++
@@ -1,8 +0,0 @@
-# Here i'm creating a remote states for terraform, this is very important when you work with a team
-terraform {
-  backend "s3" {
-    bucket = "bucket-name"
-    key    = "folder-name/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
