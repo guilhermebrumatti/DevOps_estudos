1c39fe1460298cf81da04668b9ef37c9b173c505
Guilherme Brumatti <guilhermebrumatti@yahoo.com.br>
2023-02-03 17:03:53 -0300

    Update remote-state.tf

--- terraform/Aws_alb_code/remote-state.tf
+++ terraform/Aws_alb_code/remote-state.tf
@@ -1,9 +1,8 @@
 # Aqui estou criando estados remotos para terraform, isso é muito importante quando você trabalha com uma equipe
 terraform {
   backend "s3" {
-    bucket = "bucketestudosdevops1"
+    bucket = "bucket-t00w3ewf6s65df41165sdf534112"
     key    = "terraform/Aws_alb_code/terraform.tfstate"
     region = "us-east-1"
   }
 }
-
