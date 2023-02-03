234f84c2082906200fab02832742c4dbbbb861dd
guilhermebrumatti <guilhermegnb@hotmail.com>
2023-02-03 15:18:20 -0300

    'update'

--- terraform/Aws_alb_code/bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "bucket_python" {
-    bucket = "bucketestudosdevops1"
-    acl = "private"
-
-    tags = {
-        Name        = "Bucket Python"
-        Environment = "Dev"
-    }
-}
