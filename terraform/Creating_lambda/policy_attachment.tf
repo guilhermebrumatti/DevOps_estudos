ccd13eca1a3c5124c46465adf26b910ed874193f
Guilherme Brumatti <guilhermebrumatti@yahoo.com.br>
2023-02-14 11:27:30 -0300

    Delete terraform/Creating_lambda directory

--- terraform/Creating_lambda/policy_attachment.tf
+++
@@ -1,5 +0,0 @@
-# Linking the necessary policy with role 
-resource "aws_iam_role_policy_attachment" "attachment_policy" {
- role        = aws_iam_role.lambda.name
- policy_arn  = "arn"
- }
--- terraform/Creating_lambda/remote-state.tf
+++
@@ -1,8 +0,0 @@
-# Saving the terraform status on S3
-terraform {
-  backend "s3" {
-    bucket = "name"
-    key    = "name_folder/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
